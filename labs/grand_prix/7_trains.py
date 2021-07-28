19f83114bb5d5c392c0e0a4555a4041b3b7177d2
Daniel Chuang <daniel.chuang22@avenues.org>
2021-07-28 16:28:51 -0400

    1, 2, components of 3, 7, and 9

--- labs/grand_prix/7_trains.py
+++ labs/grand_prix/7_trains.py
@@ -17,6 +17,7 @@ import numpy as np
 sys.path.insert(0, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
+from enum import Enum
 
 ########################################################################################
 # Global variables
@@ -24,7 +25,17 @@ import racecar_utils as rc_utils
 
 rc = racecar_core.create_racecar()
 
+class states(Enum):
+    stopped = 0
+    ready = 1
+    going = 2
+
+
 # Add any global variables here
+state = states.stopped
+last_distance = 1000
+speed = 0
+timer = 0
 
 ########################################################################################
 # Functions
@@ -33,14 +44,82 @@ rc = racecar_core.create_racecar()
 
 def start():
     # Have the car begin at a stop
+    rc.drive.set_max_speed(0.75)
     rc.drive.stop()
 
     # Print start message
-    print(">> Final Challenge - Grand Prix")
-
+    print(">> Grand Prix Part 7: Passing Through Trains")
 
 def update():
-    pass
+
+    """
+    PseudoCode Solution:
+
+    Four Stage Machine:
+    1. Stopped
+    2. Ready
+    3. Going
+
+    Need to be 150 distance away from train (to gather enough speed). (Stopped --> Ready)
+
+    Then, if distance suddenly changes, then accelerate. This indicates that a
+    train has just left the center. (Ready --> Going)
+
+    Finally, if something is closer than 200 distance in front of the train, then stop.
+    (Going --> Stopped)
+    """
+
+    # Defining global variables
+    global state
+    global last_distance
+    global speed
+    global timer
+
+    # Getting the three sensor inputs from the car
+    color_image = rc.camera.get_color_image()
+    depth_image = rc.camera.get_depth_image()
+    scan = rc.lidar.get_samples()
+
+    distance = scan[0]
+
+    if distance == 0:
+        distance = 10000
+    print(distance, rc.physics.get_angular_velocity()[2])
+
+    # State machine
+    if state == states.stopped:
+        if (distance > 150 and distance < 250) or distance > 400: # if the train is far away enough but close enough to actually exist
+            state = states.ready
+            print("THE STATE HAS BEEN CHANGED")
+        elif scan[360] > 30: speed = -0
+    elif state == states.ready:
+        speed = 0
+        print("distance change", distance - last_distance)
+        if last_distance - distance < -100:
+            state = state.going
+            timer = 0
+        last_distance = distance
+    else: # this is state.going
+        speed = 1
+        timer += rc.get_delta_time()
+        if timer > 0.8 or (distance < 300): #and rc.physics.get_angular_velocity()[2] > 0.5):
+            state = states.stopped
+    
+    print(state)
+
+    # !!! Manual Controls
+    """
+    rt = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
+    lt = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
+    speed = rt - lt
+    """
+
+    # Setting the speed and the angle to actually move the car
+    if speed == 0:
+        print("STOPPED")
+        rc.drive.stop()
+    else:
+        rc.drive.set_speed_angle(speed, 0)
 
 
 ########################################################################################
