21a46f7d874fcdc56a269524ec3b81f612d5bec6
joseph <jjosephlee12@gmail.com>
2021-07-29 22:52:24 -0700

    column changes

--- labs/grand_prix/columns.py
+++ labs/grand_prix/columns.py
@@ -36,11 +36,19 @@ closest_distance = 0
 contour_center = (0,0)
 timer = 0
 
+windows_left = np.array(())
+start_degrees_left = -90
+windows_right = np.array(())
+start_degrees_right = 0
+total_degrees = 90
+total_windows = 10
+max_speed = 0.8
+timer = 0
+
 class State(IntEnum):
     locate = 0
     turn_right = 1
     turn_left = 2
-    line_follow = 3
 
 cur_state: State = State.locate
 
@@ -77,6 +85,35 @@ def start(robot: racecar_core.Racecar):
     rc = robot
     # Have the car begin at a stop
     rc.drive.stop()
+    global windows_left
+    global total_degrees
+    global total_windows
+    global start_degrees_left
+    global windows_right
+    global start_degrees_right
+    global max_speed
+
+    rc.drive.set_max_speed(max_speed)
+
+    # left side
+    window_size_left = round(total_degrees / total_windows)
+    for i in range(total_windows):
+        windows_left = np.append(windows_left, i * window_size_left)
+        windows_left = np.append(windows_left, (i+1) * window_size_left - 1)
+
+    windows_left = windows_left + start_degrees_left
+    windows_left = windows_left.reshape(-1, 2)
+    print(windows_left)
+
+    # right side
+    window_size_right = round(total_degrees / total_windows)
+    for i in range(total_windows):
+        windows_right = np.append(windows_right, i * window_size_right)
+        windows_right = np.append(windows_right, (i+1) * window_size_right - 1)
+
+    windows_right = windows_right + start_degrees_right
+    windows_right = windows_right.reshape(-1, 2)
+    print(windows_right)
 
     # Print start message
     print(">> Column Turns")
@@ -87,6 +124,13 @@ def update():
     global angle
     global cur_state
     global timer
+
+    global windows_left
+    global total_degrees
+    global total_windows
+    global start_degrees_left
+    global windows_right
+    global start_degrees_right
 
     color_image = rc.camera.get_color_image()
     scan = rc.lidar.get_samples()
@@ -106,87 +150,74 @@ def update():
             row_center = (int(abs(corners[1][0] + corners[2][0]) / 2), int(abs(corners[0][1] + corners[1][1]) / 2))
             center_distance = rc_utils.get_pixel_average_distance(depth_image, row_center)
             
-            timer = 0
-            if center_distance < 150:
-                if orientation_value == 1:
-                    cur_state = State.turn_left
-                elif orientation_value == 3:
-                    cur_state = State.turn_right
-
-        
-        # if marker_id == 199:
-        #     print("hey")
-        #     pillar = update_contours()
-        #     if orientation_value == 1:
-        #         if not pillar:
-        #             angle = -0.9
-        #             speed = 1
-        #         elif pillar:
-        #             speed = 1
-        #             angle = rc_utils.remap_range(contour_center[1], 0, rc.camera.get_width(), -1, .2)
-                
-        #         if closest_distance < 100:
-        #             timer = 0
-        #             cur_state = State.turn_left
-        #     elif orientation_value == 3:
-        #         if not pillar:
-        #             angle = 0.9
-        #             speed = 1
-        #         elif pillar:
-        #             speed = 1
-        #             angle = rc_utils.remap_range(contour_center[1], 0, rc.camera.get_width(), -0.2, 1)
-                  
-        #             if closest_distance < 100:
-        #                 timer = 0
-        #                 cur_state = State.turn_right
-        # else:
-        #     angle = 0
-        #     rc.drive.set_speed_angle(speed, angle)
+            # if center_distance < 200:
+            #     print(center_distance)
+            if orientation_value == 1:
+                cur_state = State.turn_left
+            elif orientation_value == 3:
+                cur_state = State.turn_right
     
     if cur_state == State.turn_left:
         print("state left")
-        pillar = update_contours()
-        if timer < .5:
-            angle = -1
-        elif timer < 1:
-            angle = 1
-        elif timer < 3:
-            cur_state = State.locate
-        timer += rc.get_delta_time()
-        rc.drive.set_speed_angle(speed, angle)
-        # if not pillar:
-        #     timer += rc.get_delta_time()
-        #     angle = 0
+
+        # Get the (!!!average) closest distance for each window using lidar scan data
+        windows_distances_left = np.array(())
+        for window in windows_left:
+            _, window_distance_left = rc_utils.get_lidar_closest_point(scan, window)
+            windows_distances_left = np.append(windows_distances_left, window_distance_left)
+        
+        windows_distances_left = windows_distances_left.reshape(-1, 1)
+
+        # Turns to the angle
+        angle_index = np.argmax(windows_distances_left)
+        angle_degrees = np.mean(windows_left[angle_index])
+        angle = rc_utils.remap_range(angle_degrees, start_degrees_left, start_degrees_left + total_degrees - 1, -1, -.25)
+        angle = rc_utils.clamp(angle, -1, 0)
+
+        _, forward_dist = rc_utils.get_lidar_closest_point(scan, (-2, 2))
+        if forward_dist < 250 * max_speed and abs(speed) == speed:
+            multiplier = rc_utils.remap_range(forward_dist, 15 * max_speed, 250 * max_speed, 0 * max_speed, 0.7 * max_speed)
+            speed = multiplier * speed
             
-        #     if timer > .3:
-        #         timer = 0
-        #         cur_state = State.approach_blue
-        # else:
-        #     if pillar:
-        #         angle = rc_utils.remap_range(contour_center[1], 0, rc.camera.get_width(), -1, -.4)
+        rc.drive.set_speed_angle(speed, angle)
+
+        for marker in markers:
+            marker_orientation = marker.get_orientation()
+            orientation_value = marker_orientation.value
+        
+            if orientation_value == 3:
+                cur_state = State.turn_right
     
     if cur_state == State.turn_right:
         print("state right")
-        pillar = update_contours()
-        if timer < .5:
-            angle = 1
-        elif timer < 1:
-            angle = -1
-        elif timer < 3:
-            cur_state = State.locate
-        timer += rc.get_delta_time()
-        rc.drive.set_speed_angle(speed, angle)
+
+        # Get the (!!!average) closest distance for each window using lidar scan data
+        windows_distances_right = np.array(())
+        for window in windows_right:
+            _, window_distance_right = rc_utils.get_lidar_closest_point(scan, window)
+            windows_distances_right = np.append(windows_distances_right, window_distance_right)
         
-        # if not pillar:
-        #     timer += rc.get_delta_time()
-        #     angle = 0
-            
-        #     if timer > .3:
-        #         timer = 0
-        #         cur_state = State.approach_blue
-        # else:
-        #     if pillar:
-        #         angle = rc_utils.remap_range(contour_center[1], 0, rc.camera.get_width(), .4, 1)
+        windows_distances_right = windows_distances_right.reshape(-1, 1)
+
+        # Turns to the angle
+        angle_index = np.argmax(windows_distances_right)
+        angle_degrees = np.mean(windows_right[angle_index])
+        angle = rc_utils.remap_range(angle_degrees, start_degrees_right, start_degrees_right + total_degrees - 1, .25, 1)
+        angle = rc_utils.clamp(angle, 0, 1)
+
+        _, forward_dist = rc_utils.get_lidar_closest_point(scan, (-2, 2))
+        if forward_dist < 250 * max_speed and abs(speed) == speed:
+            multiplier = rc_utils.remap_range(forward_dist, 15 * max_speed, 250 * max_speed, 0 * max_speed, 0.7 * max_speed)
+            speed = multiplier * speed
+        
+        rc.drive.set_speed_angle(speed, angle)
+
+        for marker in markers:
+            marker_orientation = marker.get_orientation()
+            orientation_value = marker_orientation.value
+
+            if orientation_value == 1:
+                cur_state = State.turn_left
 
     # id = 199, left = 1, right = 3
     pass
